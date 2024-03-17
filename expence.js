fe0907dc7b1af3983a9eb563d0c6b2021255b8db
dhanapalchand <134786639+dhanapalchand@users.noreply.github.com>
2024-03-17 23:19:11 +0530

    Update expence.js

--- expence.js
+++ expence.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 const expenceSchema=new mongoose.Schema({
     amount:Number,
     desc:String,
-    title:String,
+    title:String, 
 });
 const Expence =mongoose.model('expenses',expenceSchema);
 module.exports=Expence;
